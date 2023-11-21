9387a0f5e0fd281ccfc8d55166679420daeeb80b
IvánManso <imanso.arenal@gmail.com>
2023-11-21 21:12:31 +0100

    Mercado.sol completo

--- 01. Ejercicios/02. Voumen 2/Mercado.sol
+++ 01. Ejercicios/02. Voumen 2/Mercado.sol
@@ -12,7 +12,7 @@ contract Mercado {
         address vendedor;
     }
 
-    mapping(uint256 => Producto) private productos;
+    mapping(uint256 => Producto) public productos;
     uint256 public contadorProductos;
 
     event ProductoAgregado(uint256 idProducto, string nombre, uint256 precio, uint256 cantidad, address vendedor);
@@ -33,14 +33,28 @@ contract Mercado {
         contadorProductos = 0;
     }
 
-    function agregarProducto(
-        string memory _nombre,
-        uint256 _precio,
-        uint256 _cantidad
-        ) public soloPropietario {
-            contadorProductos++;
-            productos[contadorProductos] = Producto(contadorProductos, _nombre, _precio, _cantidad, msg.sender);
-            emit ProductoAgregado(contadorProductos, _nombre, _precio, _cantidad, msg.sender);
+    function agregarProducto(string memory nombre, uint256 precio, uint256 cantidad) public soloPropietario {
+        contadorProductos++;
+        productos[contadorProductos] = Producto(contadorProductos, nombre, precio, cantidad, msg.sender);
+        emit ProductoAgregado(contadorProductos, nombre, precio, cantidad, msg.sender);
     }
 
+    function comprarProducto(uint256 idProducto) public payable productoExistente(idProducto) {
+        Producto storage producto = productos[idProducto];
+        require(producto.cantidad > 0, "Producto sin stock");
+        require(msg.value >= producto.precio, "Fondos insuficientes");
+
+        producto.cantidad--;
+        payable(producto.vendedor).transfer(producto.precio);
+        emit ProductoComprado(idProducto, producto.nombre, producto.precio, 1, msg.sender);
+    }
+
+    function obtenerProducto(uint256 idProducto) public view productoExistente(idProducto) returns (string memory, uint256, uint256, address) {
+        Producto storage producto = productos[idProducto];
+        return (producto.nombre, producto.precio, producto.cantidad, producto.vendedor);
+    }
+
+    receive() external payable {
+        // Función de respaldo para aceptar Ether
+    }
 }
