d70f0ef0a5a4d9bd7a357c06b368896a311a553b
IvánManso <imanso.arenal@gmail.com>
2023-12-14 21:05:30 +0100

    LlamadasContratosExternos.sol => updated

--- 01. Ejercicios/03. Volumen 3/Others/LlamadasContratosExternos.sol
+++ 01. Ejercicios/03. Volumen 3/Others/LlamadasContratosExternos.sol
@@ -10,13 +10,14 @@ contract ContratoDestino {
 }
 
 contract ContratoOrigen {
-    address public direccionDestino = 0x1234567890123456789012345678901234567890;
+    // address public direccionDestino = 0x1234567890123456789012345678901234567890;
 
-    function llamarSetValor(uint256 _nuevoValor) external {
+    function llamarSetValor(uint256 nuevoValor, address direccionDestino) external {
         // Aquí estamos llamando a la función setValor del ContratoDestino mediante la función call
-        (bool exito, ) = direccionDestino.call(abi.encodeWithSignature("setValor(uint256)", _nuevoValor));
-
+        (bool exito, ) = direccionDestino.call{value: 0}(abi.encodeWithSignature("setValor(uint256)", nuevoValor));
+        // <address>.call{value: xxx}(abi.encodeWithSignature(<firma_funcion>, <paramentros>) returns(bool, bytes memory)
         // Verificar si la llamada fue exitosa
+        // 
         require(exito, "La llamada a setValor no fue exitosa");
     }
 }
